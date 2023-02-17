91cdcfa03155d9f6c3028f5b62c881c3e4e3ba40
levymc <levymcruz@gmail.com>
2023-02-17 11:17:13 -0300

    Todas informacoes inseridas

--- static/javascript/buttons.js
+++ static/javascript/buttons.js
@@ -7,13 +7,15 @@ function Teste(){
         console.log(x)
         Swal.fire({
             icon: "info",
-            width:'50em',
+            width:'30em',
             confirmButtonColor:'hwb(216 31% 1%)',
             html:`
             <div class="row">
-                <div class="col"> 
-                    <p>PN Topo: ${x[0][1]}</p>
-                    <p>CEMB: ${x[0][2]}</p>
+                <div class="col" style="font-size:24px; line-height:1.75em"> 
+                    <p>PN Topo: <b>${x[0][1]}</b></p>
+                    <p>CEMB: <b>${x[0][2]}</b></p>
+                    <p>Família: <b>${x[0][6]}</b></p>
+                    <p>Relatório: <b>${x[0][7]}</b></p>
                 </div>
             </div>
             `
