fe2e0c3c841ce007d033a84c770188a663044b8b
levymc <levymcruz@gmail.com>
2023-04-19 15:50:29 -0300

    terminando a funcao confereCodigo

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -6,12 +6,15 @@ function confereCodigo(){
         }).then(response => {
             console.log(response.data);
             const divPrincipal = document.querySelector(".container");
+            divPrincipal.innerHTML = '';
             let conteudo;
-            let btnMaisInfo = `<section class="botaoInfo">Mais Informações: <a id="botaoInfo" onClick="Teste()"><i class="fa-solid fa-circle-info"></i></a></section>`
-            if (response.data[5] == "Aprovado"){
-                conteudo = `<h4 style="color:rgb(23, 184, 8)">Aprovado</h4>`;
+            let btnMaisInfo = ``;
+            console.log(response.data[0][5])
+            if (response.data[0][5] == "Aprovado"){
+                conteudo = `<div class="col-sm"><h4 style="color:rgb(23, 184, 8)">Aprovado</h4>
+                <section class="botaoInfo">Mais Informações: <a id="botaoInfo" onClick="Teste()"><i class="fa-solid fa-circle-info"></i></a></section></div>`;
             }else{
-                conteudo = `<h4 style="color:rgb(211, 7, 7)">Não Aprovado</h4>`;
+                conteudo = `<div class="col-sm"><h4 style="color:rgb(211, 7, 7)">Não Aprovado</h4></div>`;
             }
             divPrincipal.innerHTML+=`
                 <div class="cabecalho">
@@ -22,7 +25,7 @@ function confereCodigo(){
                 <div class="conteudo">
                     <div class="col-sm"><p style="font-size:18px">${response.data[0][1]}</p></div>
                     <div class="col-sm"><p style="font-size:18px">${response.data[0][2]}</p></div>
-                    <div class="col-sm status">${conteudo}</div>
+                    ${conteudo}
                 </div>`
         }).catch(error => {
             console.log(error);
