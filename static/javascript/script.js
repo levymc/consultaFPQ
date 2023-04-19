55135c171c9ed1116281f599dd181b101011c375
levymc <levymcruz@gmail.com>
2023-04-19 15:46:57 -0300

    retornando o status e com cor

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -5,6 +5,25 @@ function confereCodigo(){
             valor: input.value,
         }).then(response => {
             console.log(response.data);
+            const divPrincipal = document.querySelector(".container");
+            let conteudo;
+            let btnMaisInfo = `<section class="botaoInfo">Mais Informações: <a id="botaoInfo" onClick="Teste()"><i class="fa-solid fa-circle-info"></i></a></section>`
+            if (response.data[5] == "Aprovado"){
+                conteudo = `<h4 style="color:rgb(23, 184, 8)">Aprovado</h4>`;
+            }else{
+                conteudo = `<h4 style="color:rgb(211, 7, 7)">Não Aprovado</h4>`;
+            }
+            divPrincipal.innerHTML+=`
+                <div class="cabecalho">
+                    <div class="col-sm"><h2>PN Topo</h2></div>
+                    <div class="col-sm"><h2>Código Embraer</h2></div>
+                    <div class="col-sm"><h2>STATUS</h2></div>
+                </div>
+                <div class="conteudo">
+                    <div class="col-sm"><p style="font-size:18px">${response.data[0][1]}</p></div>
+                    <div class="col-sm"><p style="font-size:18px">${response.data[0][2]}</p></div>
+                    <div class="col-sm status">${conteudo}</div>
+                </div>`
         }).catch(error => {
             console.log(error);
         })
