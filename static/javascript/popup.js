ff7dc88f5326cc6aa4f771d6b0643333c315e2f9
levymc <levymcruz@gmail.com>
2023-06-13 09:58:03 -0300

    att

--- static/javascript/popup.js
+++ static/javascript/popup.js
@@ -1,10 +1,7 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     // document.getElementById("sideBtn").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+}
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     // document.getElementById("sideBtn").style.marginLeft = "0";
